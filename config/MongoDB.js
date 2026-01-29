dc9b0a5055daafa9e2cc227ba8249c4f78e98f3c
ryadav00 <ryadav04249@gmail.com>
2026-01-29 13:11:04 +0530

    change mongodb url

--- config/MongoDB.js
+++ config/MongoDB.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 
 async function Connection() {
-   await mongoose.connect("mongodb://localhost:27017/JWTAUTH").then(()=>{
+   await mongoose.connect("mongodb+srv://Sumit9625:GQzdSIuPymr3dZSD@sumit.xro5h.mongodb.net/jwtWork").then(()=>{
         console.log("MongoDB connected succsessfuly")
     }).catch((e)=>{
  console.log(e)
